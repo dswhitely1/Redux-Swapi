0db06ae316f25e709a826c3b7ceaac8c9d0b255e
Donald Whitely <dswhitely1@gmail.com>
2019-06-20 09:44:32 -0400

    MVP Complete

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,35 +1,38 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { CharacterList } from "../components";
+import { CharacterList } from '../components';
 // import actions
+import { fetchStarWarsPeople } from '../actions';
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  componentDidMount() {
-    // call our action
-  }
+	componentDidMount() {
+		// call our action
+		this.props.fetchStarWarsPeople();
+	}
 
-  render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
-    }
-    return (
-      <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
-      </div>
-    );
-  }
+	render() {
+		if (this.props.fetching) {
+			// return something here to indicate that you are fetching data
+		}
+		return (
+			<div className='CharactersList_wrapper'>
+				<CharacterList characters={this.props.characters} />
+			</div>
+		);
+	}
 }
 
+const mapStateToProps = state => ({
+	characters : state.charsReducer.characters,
+	fetching   : state.charsReducer.isFetching,
+});
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
-export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
-)(CharacterListView);
+export default connect(mapStateToProps /* mapStateToProps replaces null here */, {
+	fetchStarWarsPeople /* action creators go here */,
+})(CharacterListView);
